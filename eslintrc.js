c96c834610966480f9af5f0715be9a8a5d1e6698
Hakan Hueriyet <hurriyet@fastmail.com>
2021-03-24 10:25:51 +0100

    initial commit

---
+++ eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['./core.js'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  reportUnusedDisableDirectives: true,
+  root: true
+};
---
+++ react-typescript.js
@@ -0,0 +1,61 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    './core.js',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    './prettier-react-typescript.js',
+    './react.js'
+  ],
+  plugins: ['import', '@typescript-eslint'],
+  rules: {
+    // Import
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    // TypeScript
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    // Other
+    radix: 'warn',
+    // Overrides for ESLINT config
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error'
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src']
+      }
+    }
+  }
+};
